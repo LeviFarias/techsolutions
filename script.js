ed822ebcbeb57613f0da157035b6561e0267e2e3
LeviFarias <110645340+LeviFarias@users.noreply.github.com>
2024-06-05 23:25:26 -0300

    Atualização Geral do Site

--- script.js
+++ script.js
@@ -13,29 +13,3 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
-
-
-document.addEventListener("DOMContentLoaded", function() {
-    var element = document.querySelector(".hover-underline-animation");
-    var pattern = document.querySelector(".pattern");
-
-    window.addEventListener("scroll", function() {
-        if (isScrolledIntoView(pattern)) {
-            element.classList.add("scrolled");
-        } else {
-            element.classList.remove("scrolled");
-        }
-    });
-
-    function isScrolledIntoView(el) {
-        var rect = el.getBoundingClientRect();
-        var elemTop = rect.top;
-        var elemBottom = rect.bottom;
-
-        // Only completely visible elements return true:
-        var isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
-        // Partially visible elements return true:
-        // isVisible = elemTop < window.innerHeight && elemBottom >= 0;
-        return isVisible;
-    }
-});
